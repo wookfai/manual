7cc2e72da6f8949bc175ef814f7b0d9d34214b5e
wookfai <wookfai@gmail.com>
2024-06-16 17:01:40 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Symphony eTQM'
+copyright = '2024, BusinessQuest.com Sdn Bhd'
+author = 'bizquest'
 
 release = '0.1'
 version = '0.1.0'
